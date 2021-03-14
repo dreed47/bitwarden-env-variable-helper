1dd6c18a03d71b8c2b905f9365fc618989e6fed1
David Reed <212698+dreed47@users.noreply.github.com>
2021-03-14 16:39:09 -0400

    updates

--- export-variables.sh
+++ export-variables.sh
@@ -6,6 +6,6 @@ for i in "${arrIN[@]}"
 do
     :
     #echo "$i"
-    echo "$i"="kkkkk" >> $GITHUB_ENV
-    #echo "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
+    #echo "$i"="kkkkk" >> $GITHUB_ENV
+    echo "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
 done
