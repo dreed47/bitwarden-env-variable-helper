897997d0ed7f5549433947c698fdaf67f64ccae1
David Reed <212698+dreed47@users.noreply.github.com>
2021-03-14 16:12:31 -0400

    updates

--- export-variables.sh
+++ export-variables.sh
@@ -1,2 +1,11 @@
 echo "Hello All-Yall!!!!!!"
 echo "input is $1"
+
+IFS='|' read -ra arrIN <<< "$1"
+for i in "${arrIN[@]}"
+do
+    :
+    echo "$i"
+    echo "$i"="huhuhuhuuuuuii" >> $GITHUB_ENV
+    #echo "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
+done
