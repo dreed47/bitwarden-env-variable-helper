cbc0481e83b0482bbd4d1d77773d580332c93579
David Reed <212698+dreed47@users.noreply.github.com>
2021-03-17 15:22:13 -0400

    hhh

--- export-variables.sh
+++ export-variables.sh
@@ -10,7 +10,10 @@ do
     :
     #echo "$i"
     #echo "$i"="kkkkk" >> $GITHUB_ENV
-    echo "$i=$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
+    echo "$i<<EOF" >> $GITHUB_ENV
+    echo "$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
+    echo "EOF" >> $GITHUB_ENV
+    #echo "$i=$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
     #echo -e "$i=\n$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
     #echo -e "$i='$(bw list items --search $i | jq '.[0] | .notes' -r)'" >> ${GITHUB_ENV}
     #printf "%s" "ID_RSA_PRIVATE="$(bw list items --search ID_RSA_PRIVATE | jq '.[0] | .notes' -r) >> testfile
