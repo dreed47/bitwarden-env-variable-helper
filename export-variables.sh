c898465152dfaa0d82a063c3a7db997da6eccab6
David Reed <212698+dreed47@users.noreply.github.com>
2021-03-17 14:22:48 -0400

    uuu

--- export-variables.sh
+++ export-variables.sh
@@ -1,5 +1,4 @@
 echo "Export variables to GITHUB_ENV"
-echo "Access these variables with ${{ env.SOMEVARIABLE }}"
 echo "input is $1"
 
 # sync BitWarden to ensure the latest content
@@ -12,7 +11,10 @@ do
     #echo "$i"
     #echo "$i"="kkkkk" >> $GITHUB_ENV
     #echo "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
-    echo "$i=$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
+    export i="ID_RSA_PRIVATE"
+    export GITHUB_ENV="testfile1"
+
+    echo -e "$i=\n$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
     #printf "%s" "ID_RSA_PRIVATE="$(bw list items --search ID_RSA_PRIVATE | jq '.[0] | .notes' -r) >> testfile
     #printenv "ID_RSA_PRIVATE="$(bw list items --search ID_RSA_PRIVATE | jq '.[0] | .notes' -r) >> $GITHUB_ENV
     #printenv "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
