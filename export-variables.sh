c87e26bdbd46aef40828ac6a335603246902d95a
David Reed <212698+dreed47@users.noreply.github.com>
2021-03-17 14:40:51 -0400

    kkk

--- export-variables.sh
+++ export-variables.sh
@@ -11,7 +11,8 @@ do
     #echo "$i"
     #echo "$i"="kkkkk" >> $GITHUB_ENV
     #echo "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
-    echo -e "$i=\n$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
+    #echo -e "$i=\n$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
+    echo -e "$i=terraform\n$(bw list items --search $i | jq '.[0] | .notes' -r)" >> ${GITHUB_ENV}
     #printf "%s" "ID_RSA_PRIVATE="$(bw list items --search ID_RSA_PRIVATE | jq '.[0] | .notes' -r) >> testfile
     #printenv "ID_RSA_PRIVATE="$(bw list items --search ID_RSA_PRIVATE | jq '.[0] | .notes' -r) >> $GITHUB_ENV
     #printenv "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
