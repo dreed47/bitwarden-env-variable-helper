ab75d96f94138157971a9189df41c748a56722ce
david <212698+dreed47@users.noreply.github.com>
2021-03-17 21:16:38 -0400

    h

--- export-variables.sh
+++ export-variables.sh
@@ -8,7 +8,7 @@ IFS='|' read -ra arrIN <<< "$1"
 for i in "${arrIN[@]}"
 do
     :
-echo "$i=$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
+echo "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
 
 done
     #echo "$i"
