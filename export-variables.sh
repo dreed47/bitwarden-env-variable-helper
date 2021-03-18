59463d29012918d4d4a56a6fa13edb954990ddc7
david <212698+dreed47@users.noreply.github.com>
2021-03-17 23:01:28 -0400

    s

--- export-variables.sh
+++ export-variables.sh
@@ -12,14 +12,4 @@ do
     bw list items --search $i | jq '.[0] | .notes' -r >> $GITHUB_ENV
     echo "EOF" >> $GITHUB_ENV
 
-    #echo "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
-
 done
-    #echo "$i"
-    #echo "$i=$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
-    #echo -e "$i=\n$(bw list items --search $i | jq '.[0] | .notes' -r)" >> $GITHUB_ENV
-    #echo -e "$i='$(bw list items --search $i | jq '.[0] | .notes' -r)'" >> ${GITHUB_ENV}
-    #printf "%s" "ID_RSA_PRIVATE="$(bw list items --search ID_RSA_PRIVATE | jq '.[0] | .notes' -r) >> testfile
-    #printenv "ID_RSA_PRIVATE="$(bw list items --search ID_RSA_PRIVATE | jq '.[0] | .notes' -r) >> $GITHUB_ENV
-    #printenv "$i="$(bw list items --search $i | jq '.[0] | .notes' -r) >> $GITHUB_ENV
-    #echo "ID_RSA_PRIVATE=$(bw list items --search ID_RSA_PRIVATE | jq '.[0] | .notes' -r)" >> testfile
