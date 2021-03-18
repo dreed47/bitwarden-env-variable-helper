4bc5c6db38f81252b2aa92db8dce28ba5c9d44aa
David Reed <212698+dreed47@users.noreply.github.com>
2021-03-18 14:53:52 -0400

    ggg

--- export-variables.sh
+++ export-variables.sh
@@ -1,5 +1,6 @@
 echo "Export variables to GITHUB_ENV"
-echo "input is $1"
+echo "key list= $1"
+echo "prefix is $2"
 
 # sync BitWarden to ensure the latest content
 bw sync
@@ -9,7 +10,7 @@ for i in "${arrIN[@]}"
 do
     :
     echo "$i<<EOF" >> $GITHUB_ENV
-    bw list items --search $i | jq '.[0] | .notes' -r >> $GITHUB_ENV
+    bw list items --search $2$i | jq '.[0] | .notes' -r >> $GITHUB_ENV
     echo "EOF" >> $GITHUB_ENV
 
 done
